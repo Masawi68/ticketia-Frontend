e5f3e6666c121965321fae1d8ab6614e46fa34b7
Masawi <edmasawi@ymail.com>
2025-02-04 15:36:44 +0100

    package.json & vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Use the PORT environment variable or default to 5173
+  },
 })
