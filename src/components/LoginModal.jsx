efab95a197bd2d178455746c768519dd62bda6c0
Masawi <edmasawi@ymail.com>
2025-02-04 14:24:27 +0100

    back url setup1

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -27,7 +27,7 @@ const LoginModal = ({ isOpen, toggleModal, onLogin }) => {
     }
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://ticketia-backend.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
